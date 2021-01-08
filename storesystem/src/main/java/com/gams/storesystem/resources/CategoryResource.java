dc24790f66a4b0c6544691939e93a39518928a1d
gams <gabriel.antonioms@gmail.com>
2021-01-08 20:25:19 -0300

    Testing the first domain class

--- storesystem/src/main/java/com/gams/storesystem/resources/CategoryResource.java
+++ storesystem/src/main/java/com/gams/storesystem/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.gams.storesystem.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gams.storesystem.domain.Category;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoryResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST is running!";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+ 		list.add(cat2);
+ 		
+		return list;
 	}
 }
