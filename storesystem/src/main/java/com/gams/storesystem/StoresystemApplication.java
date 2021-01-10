258b1e354c8567f6e4306da050a1d34006ac0aa1
gams <gabriel.antonioms@gmail.com>
2021-01-10 03:05:17 -0300

    creating data injection operation

--- storesystem/src/main/java/com/gams/storesystem/StoresystemApplication.java
+++ storesystem/src/main/java/com/gams/storesystem/StoresystemApplication.java
@@ -1,13 +1,33 @@
 package com.gams.storesystem;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class StoresystemApplication {
+import com.gams.storesystem.domain.Category;
+import com.gams.storesystem.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class StoresystemApplication implements CommandLineRunner {
+
+	@Autowired //data injection
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(StoresystemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2)); //save and inject data in db
+		
 	}
 
 }
