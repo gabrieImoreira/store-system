95a36201dd09563c713892be539a74b00bcd88ca
gams <gabriel.antonioms@gmail.com>
2021-01-12 02:52:58 -0300

    Created States and Cities classes

--- storesystem/src/main/java/com/gams/storesystem/StoresystemApplication.java
+++ storesystem/src/main/java/com/gams/storesystem/StoresystemApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gams.storesystem.domain.Category;
+import com.gams.storesystem.domain.City;
 import com.gams.storesystem.domain.Product;
+import com.gams.storesystem.domain.State;
 import com.gams.storesystem.repositories.CategoryRepository;
+import com.gams.storesystem.repositories.CityRepository;
 import com.gams.storesystem.repositories.ProductRepository;
+import com.gams.storesystem.repositories.StateRepository;
 
 @SpringBootApplication
 public class StoresystemApplication implements CommandLineRunner {
@@ -19,6 +23,10 @@ public class StoresystemApplication implements CommandLineRunner {
 	private CategoryRepository categoryRepository;
 	@Autowired 
 	private ProductRepository productRepository;
+	@Autowired 
+	private CityRepository cityRepository;
+	@Autowired 
+	private StateRepository stateRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(StoresystemApplication.class, args);
@@ -34,6 +42,13 @@ public class StoresystemApplication implements CommandLineRunner {
 		Product p2 = new Product(null, "Printer", 800.0);
 		Product p3 = new Product(null, "Mouse", 80.0);
 		
+		State st1 = new State(null, "Minas Gerais");
+		State st2 = new State(null, "São Paulo");
+		
+		City c1 = new City(null, "Uberlândia", st1);
+		City c2 = new City(null, "São Paulo", st2);
+		City c3 = new City(null, "Campinas", st2);
+		
 		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProducts().addAll(Arrays.asList(p2));
 		
@@ -41,8 +56,15 @@ public class StoresystemApplication implements CommandLineRunner {
 		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategories().addAll(Arrays.asList(cat1));
 		
+		st1.getCities().addAll(Arrays.asList(c1));
+		st2.getCities().addAll(Arrays.asList(c2, c3));
+		
+	
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2)); //save and inject data in db
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		stateRepository.saveAll(Arrays.asList(st1, st2));
+		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
 	}
 
 }
